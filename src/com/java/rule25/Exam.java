3570df95acf233321fed7eb4fa3db10ada078d93
USER <user@AL01289094.local>
2020-02-09 01:13:23 +0900

    add : enum exam

---
+++ src/com/java/rule25/Exam.java
@@ -0,0 +1,15 @@
+package com.java.rule25;
+
+import com.java.rule13.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exam {
+    public static void main(String[] args){
+//        List<Object> ol = new ArrayList<Long>();
+//        ol.add("I don't fit in");
+        Object[] objectArray = new Long[1];
+        objectArray[0] = "I don' t fit in";
+    }
+}
